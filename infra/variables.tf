eada1f99e15cd675fa785ca022abe36696686695
gjfeel4512-creator <gjfeel4512@gmail.com>
2026-08-27 10:42:52 +0900

    Feat: 누락 오전분

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,10 +4,11 @@ variable "aws_region" {
   default     = "ap-northeast-2"
 }
 
+# 프로젝트명 수정 : de-ai-14-infra => de-ai-14-loggen
 variable "project_name" {
   description = "데이터 엔지니터 프로젝트 연습용"
   type        = string
-  default     = "de-ai-14-infra"
+  default     = "de-ai-14-loggen"
 }
 
 variable "environment" {
@@ -20,5 +21,14 @@ variable "environment" {
 variable "s3_force_destroy" {
   description = "True면 버킷 내부 데이터 모두 삭제하고, 버킷까지 삭제"
   type        = bool
-  default     = true
+  default     = false
+}
+
+# s3.tf가 없다면 -> 기존에 존재하는 버킷을 사용하여 처리하는 방식
+variable "silver_bucket_name" {
+  description = "기존 silver parquet 데이터가 실제 저장하고 있는 s3 버킷 이름 입력"
+  type        = string
+  # 일단 고정으로 사용
+  default = "de-ai-14-loggen-s3-bk-827913617635"
+  # default 누락시키면 => plan or apply 하면 사용자에게 물어봄(입력대기. 사용자와 인터렉션 가능)
 }
